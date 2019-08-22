fca52b4a083de2da04c821f8332ff6ca49e287b9
zhmhClark <778970072@qq.com>
2019-08-22 11:53:48 +0800

    tab

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -2,16 +2,19 @@ package cell;
 
 import java.awt.Graphics;
  
-public class Cell {
+public class Cell 
+{
 	private boolean alive = false;
 	
 	public void die() { alive = false; }
 	public void reborn() { alive = true; }
 	public boolean isAlive() { return alive; }
 	
-	public void draw(Graphics g, int x, int y, int size) {
+	public void draw(Graphics g, int x, int y, int size) 
+	{
 		g.drawRect(x, y, size, size);
-		if ( alive ) {
+		if ( alive ) 
+		{
 			g.fillRect(x, y, size, size);
 		}
 	}
